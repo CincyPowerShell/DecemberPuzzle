ceed8bfd1742f33384b690856c25a2aa2eb6a5e0
bruckect <computerwiz.89@gmail.com>
2015-12-08 20:44:06 -0500

    Create PuzzleCode.ps1

---
+++ PuzzleCode.ps1
@@ -0,0 +1,14 @@
+$list = @"
+1 Partridge in a pear tree
+2 Turtle Doves
+3 French Hens
+4 Calling Birds
+5 Golden Rings
+6 Geese a laying
+7 Swans a swimming
+8 Maids a milking
+9 Ladies dancing
+10 Lords a leaping
+11 Pipers piping
+12 Drummers drumming
+"@
